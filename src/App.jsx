ea11654e27fe4ebfd4646855b955b025a149c1fd
Ali Hamas <alihamasghurki4@gmail.com>
2024-07-14 02:23:32 +0500

    basic router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { Toaster } from "react-hot-toast";
+import { Auth, SignUp, SignIn } from "@/pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Feed, Bookmarks, Users, Profile, Tweet } from "@/pages";
+
 function App() {
-  return <div className="text-center flex-center">APP</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Feed />}></Route>
+          <Route path="/bookmarks" element={<Bookmarks />}></Route>
+          <Route path="/users" element={<Users />}></Route>
+          <Route path="/users/:username" element={<Profile />}></Route>
+          <Route path="/tweets" element={<Feed />}></Route>
+          <Route path="/tweets/:tweetId" element={<Tweet />}></Route>
+          <Route path="/" element={<Auth />}>
+            <Route path="signup" element={<SignUp />}></Route>
+            <Route path="signin" element={<SignIn />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          duration: 3000,
+          className: "font-medium font-chirp",
+          style: {
+            background: "rgb(var(--accentColor))",
+            color: "#fff",
+          },
+        }}
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import Auth from "./Auth";
+import Feed from "./Feed";
+import Profile from "./Profile";
+import Tweet from "./Tweet";
+import Bookmarks from "./Bookmarks";
+import Users from "./Users";
+
+export { SignIn, SignUp, Auth, Feed, Profile, Tweet, Bookmarks, Users };
