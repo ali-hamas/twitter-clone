2b9b3b2f847624b1432c04d35ad966fa16e6b8c1
Ali Hamas <alihamasghurki4@gmail.com>
2024-07-18 03:31:33 +0500

    initial setup complete of feed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Header } from "@/layout";
+import { useEffect, useState } from "react";
+import { useAuth } from "@/contexts";
+import { Form, Tweet } from "@/components/tweet";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const { tweets } = useAuth();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    document.title = "Home / Twitter";
+  }, []);
+
+  console.log("useTweets changed");
+  return (
+    <>
+      <Header title="Home" back={false} />
+      <section className="w-full">
+        <Form />
+      </section>
+      <section className="w-full pb-96">
+        {tweets.map((data) => (
+          <Tweet key={data.$id} data={data} />
+        ))}
+      </section>
+    </>
+  );
 };
 
 export default Feed;
